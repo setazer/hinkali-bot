f35ab839b3a9e5491f46d4df728ea0003e835887
setazer <we.are@setazer.us>
2019-11-21 20:45:09 +0500

    add payments improvements

--- main.py
+++ main.py
@@ -22,13 +22,17 @@ WEBHOOK_URL = f"https://{DOMAIN}{WEBHOOK_PATH}"
 
 bot = Bot(token=TELEGRAM_TOKEN)
 bot.users = {}
+bot.payments = set()
 bot.orders = {}
+bot.organizer = None
 bot.order_message = None
+bot.payments_message = None
 bot.discount = 30
 dp = Dispatcher(bot)
 
 hin_cb = CallbackData('hin', 'type', 'amount')
 disc_cb = CallbackData('discount', 'multiplier')
+pay_cb = CallbackData('payment', 'status')
 
 h_types = {'ВК': 50, 'ВБ': 50, 'ЖК': 55, 'ЖБ': 55}
 h_amounts = ('-4', '-2', '-1', '0', '+1', '+2', '+4')
@@ -40,14 +44,6 @@ def ignored(exc):
         yield
     except exc:
         pass
-
-
-def pointer_markup():
-    markup = types.InlineKeyboardMarkup()
-    link_chat_id = str(bot.order_message.chat.id).replace("-100", "")
-    url = f'https://t.me/c/{link_chat_id}/{bot.order_message.message_id}'
-    markup.add(types.InlineKeyboardButton(text='Ссылка на заказ', url=url))
-    return markup
 
 
 def hinkali_markup():
@@ -65,50 +61,84 @@ def hinkali_markup():
     return markup
 
 
-def count_user(user):
-    orders = bot.orders[user]
-    total = sum(map(lambda x: orders.get(x) * h_types[x] * (100 - bot.discount) / 100, orders))
-    return total
+def pointer_markup():
+    markup = types.InlineKeyboardMarkup()
+    link_chat_id = str(bot.order_message.chat.id).replace("-100", "")
+    url = f'https://t.me/c/{link_chat_id}/{bot.order_message.message_id}'
+    markup.add(types.InlineKeyboardButton(text='Ссылка на заказ', url=url))
+    return markup
 
 
-def gen_report():
+def payments_markup():
+    markup = types.InlineKeyboardMarkup()
+    markup.row(
+        types.InlineKeyboardButton(text=emojize(':dollar: Оплачено'),
+                                   callback_data=pay_cb.new(status='paid')),
+        types.InlineKeyboardButton(text=emojize(':money_with_wings: Отмена'),
+                                   callback_data=pay_cb.new(status='canceled')),
+    )
+    return markup
+
+
+def payment_report():
+    lines = []
+    if bot.organizer:
+        lines.append(f"Орг: {bot.users[bot.organizer]}")
+        payers = set(bot.orders) - {bot.organizer}
+    else:
+        payers = tuple(bot.orders)
+    for user_id in payers:
+        mark = emojize(':white_check_mark:' if user_id in bot.payments else ':x:')
+        lines.append(f"{mark} {bot.users[user_id]}")
+    report = '\n'.join(lines)
+    return report
+
+
+def order_report():
     lines = ['Заказ:']
     total_cost = 0
 
     if not bot.orders:
         order = 'Ещё ничего не заказано'
         return order
-    for user, user_orders in bot.orders.items():
-        name = bot.users[user]
-        user_cost = count_user(user)
+    for user_id, user_orders in bot.orders.items():
+        name, orders = bot.users[user_id], bot.orders[user_id]
+        user_cost = sum(map(lambda x: orders[x] * h_types[x] * (100 - bot.discount) / 100, orders))
         orders = ", ".join([f'{str(amount)} {h_type}' for h_type, amount in user_orders.items()])
         lines.append(f'{name} ({user_cost} ₽): {orders}')
         total_cost += user_cost
-    total_orders = {h_type: sum(user_orders.get(h_type, 0) for user_orders in bot.orders.values()) for h_type in h_types}
-    total_orders_str = ", ".join([f'{str(amount)} {h_type}' for h_type, amount in total_orders.items() if amount])
-    lines.append('\nВсего заказ: {}'.format(total_orders_str))
-    lines.append(f'Итого: {total_cost} ₽')
+    total_orders = {h_type: sum(user_orders.get(h_type, 0)
+                                for user_orders in bot.orders.values())
+                    for h_type in h_types}
+    total_orders_str = ", ".join([f'{str(amount)} {h_type}'
+                                  for h_type, amount in total_orders.items()
+                                  if amount])
+    lines.append(f'\nИтого: {total_cost} ₽ - {total_orders_str}')
     order = '\n'.join(lines)
     return order
 
 
 @dp.callback_query_handler(hin_cb.filter())
 async def amount_change(call: types.CallbackQuery, callback_data):
-    user, name = call.from_user.id, call.from_user.full_name or call.from_user.username
-    h_type, amount = callback_data['type'], int(callback_data['amount'])
-    bot.users.setdefault(user, name)
-    cur_orders = bot.orders.setdefault(user, {})
-    cur_amount = cur_orders.setdefault(h_type, 0) + amount
+    user_id, name = call.from_user.id, call.from_user.full_name or call.from_user.username
+    h_type, amount = callback_data['type'], callback_data['amount']
+    bot.users.setdefault(user_id, name)
+    cur_orders = bot.orders.setdefault(user_id, {})
+    cur_amount = cur_orders.setdefault(h_type, 0) + int(amount)
     if cur_amount <= 0:
         del cur_orders[h_type]
         if not cur_orders:
-            del bot.orders[user]
-            del bot.users[user]
+            del bot.orders[user_id]
+            del bot.users[user_id]
     else:
         cur_orders[h_type] = cur_amount
     try:
-        await bot.edit_message_text(gen_report(), call.message.chat.id,
-                                    call.message.message_id, reply_markup=hinkali_markup())
+
+        order_message = await bot.edit_message_text(order_report(), call.message.chat.id,
+                                                    call.message.message_id, reply_markup=hinkali_markup())
+        if not bot.order_message:
+            bot.order_message = order_message
+        await bot.answer_callback_query(call.id, amount)
     except MessageNotModified:
         await bot.answer_callback_query(call.id)
 
@@ -118,9 +148,29 @@ async def discount_change(call: types.CallbackQuery, callback_data):
     chat_id, mess_id = call.message.chat.id, call.message.message_id
     bot.discount = int(callback_data['multiplier'])
     if bot.orders:
-        await bot.edit_message_text(gen_report(), chat_id, mess_id, reply_markup=hinkali_markup())
+        order_message = await bot.edit_message_text(order_report(), chat_id, mess_id, reply_markup=hinkali_markup())
+        if not bot.order_message:
+            bot.order_message = order_message
     else:
         await bot.edit_message_reply_markup(chat_id, mess_id, reply_markup=hinkali_markup())
+
+
+@dp.callback_query_handler(pay_cb.filter())
+async def payment_change(call: types.CallbackQuery, callback_data):
+    user_id, status = call.from_user.id, callback_data['status']
+    chat_id, mess_id = call.message.chat.id, call.message.message_id
+    if status == 'paid':
+        bot.payments.add(user_id)
+    elif status == 'canceled':
+        with ignored(KeyError):
+            bot.payments.remove(user_id)
+    try:
+        payments_message = await bot.edit_message_text(payment_report(), chat_id, mess_id, reply_markup=payments_markup())
+        if not bot.payments_message:
+            bot.payments_message = payments_message
+        await bot.answer_callback_query(call.id, "Учтено")
+    except MessageNotModified:
+        await bot.answer_callback_query(call.id)
 
 
 @dp.message_handler(ChatType.is_group_or_super_group, commands=['hinkali'])
@@ -145,10 +195,17 @@ async def get_organizer(message: types.Message):
             await asyncio.sleep(5)
             await bot.delete_message(warning.chat.id, warning.message_id)
         return
-    user_id = random.choice(tuple(bot.orders))
-    name = bot.users[user_id]
+    bot.organizer = random.choice(tuple(bot.orders))
+    name = bot.users[bot.organizer]
     await bot.send_message(chat_id, f"Святым рандомом заказывающим назначается:\n"
-                                    f"[{name}](tg://user?id={user_id})", parse_mode='markdown')
+                                    f"[{name}](tg://user?id={bot.organizer})", parse_mode='markdown')
+
+
+@dp.message_handler(ChatType.is_group_or_super_group, commands=['pay'])
+async def make_payments(message: types.Message):
+    chat_id, mess_id = message.chat.id, message.message_id
+    if not bot.payments_message:
+        bot.payments_message = await bot.send_message(chat_id, payment_report(), reply_markup=payments_markup())
 
 
 @dp.message_handler(ChatType.is_group_or_super_group, commands=['finish'])
@@ -158,13 +215,20 @@ async def finalize(message: types.Message):
     chat_id, mess_id = bot.order_message.chat.id, bot.order_message.message_id
     with ignored(BadRequest):
         await bot.edit_message_reply_markup(chat_id, mess_id, reply_markup=None)
+    if bot.payments_message:
+        chat_id, mess_id = bot.payments_message.chat.id, bot.payments_message.message_id
+        with ignored(BadRequest):
+            await bot.edit_message_reply_markup(chat_id, mess_id, reply_markup=None)
     if bot.orders:
-        mentions=', '.join([f"[{bot.users[user_id]}](tg://user?id={user_id})" for user_id in bot.orders])
+        mentions = ', '.join([f"[{bot.users[user_id]}](tg://user?id={user_id})" for user_id in bot.orders])
         await bot.send_message(chat_id, f"Заказ приехал!\n{mentions}", parse_mode='markdown')
     bot.order_message = None
+    bot.payments_message = None
     bot.users = {}
     bot.orders = {}
-    
+    bot.payments = set()
+    bot.organizer = None
+
 
 @dp.message_handler(ChatType.is_private, commands=['start'])
 async def start(message: types.Message):
